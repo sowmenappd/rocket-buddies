b0742faf085ce2f521e81a66a3ab15e014787e48
sowmenappd <sowmen.appd@gmail.com>
2019-11-24 00:01:02 +0600

    added test code for vertex displacement propagation, and fixed vertex drawing bug

--- Assets/Scripts/MapBuilder.cs
+++ Assets/Scripts/MapBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,33 +8,54 @@ using UnityEngine;
 public class MapBuilder : MonoBehaviour
 {   
     public bool useRandomSeed;
-    public int seedValue;
+    public float seedValue;
     public int mapSizeX, mapSizeZ;
     
     public Mesh mapMesh;
 
+    int counter = 0;
+    public bool propagateForward = false;
 
     [Range(.05f, 10f)] public float vertexPlacementDistance;
     [Range(.05f, 1f)] public float noiseScale;
     
     void Update(){
+        // counter = (counter + 1) % 3;
         if(Input.GetKeyDown(KeyCode.Space)){
             GenerateMesh();
         }
+        // if(mapMesh && propagateForward && counter == 0){
+        //     mapMesh.vertices = PropagateValuesForward(mapMesh.vertices);
+        // }
+    }
+
+    //TESTING
+    Vector3[] PropagateValuesForward(Vector3[] vertices)
+    {
+        for(int z = mapSizeZ - 1; z >= 0; z--){
+            float endVal = vertices[z * mapSizeX + mapSizeX - 1].y;
+            for(int x = mapSizeX - 1; x >= 1; x--){
+                    vertices[z * mapSizeX + x].y = vertices[z * mapSizeX + x - 1].y;
+            }
+            vertices[z * mapSizeX].y = endVal;   
+        }
+        return vertices;
     }
 
     void GenerateMesh(){
         var seed = seedValue;
         if(useRandomSeed){
-            seed = seedValue = Random.Range(-4, 5);
+            seed = seedValue = UnityEngine.Random.Range(-4f, 5f);
         }   
         mapMesh = MeshGenerator.CreateMesh(transform, mapSizeX, mapSizeZ, noiseScale, vertexPlacementDistance, seedValue); 
+        mapMesh.RecalculateNormals();
+        //mapMesh.RecalculateTangents();
     }
 
-    void OnDrawGizmos(){
+    void OnDrawGizmosSelected(){
         if(mapMesh){
             foreach(Vector3 v in mapMesh.vertices){
-                Gizmos.DrawSphere(v, 0.01f);
+                Gizmos.DrawSphere(v, 0.05f);
             }
         }
 
--- Assets/Scripts/Terrain Generation/MeshGenerator.cs
+++ Assets/Scripts/Terrain Generation/MeshGenerator.cs
@@ -1,44 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public static class MeshGenerator
 {
-    public static Mesh CreateMesh(Transform target, int sizeX, int sizeZ, float noiseScale, float adjacentPointDistance, int randomSeed = -1){
+    public static Mesh CreateMesh(Transform target, int sizeX, int sizeZ, float noiseScale, float adjacentPointDistance, float randomSeed = -1){
         MeshFilter meshFilter = target.GetComponent<MeshFilter>();
         MeshRenderer renderer = target.GetComponent<MeshRenderer>();
 
         Mesh mesh = new Mesh();
         mesh.name = "Auto-generated Mesh";
 
-        Vector3[] vertices = CreateVertices(target.position, sizeX, sizeZ, noiseScale, adjacentPointDistance, randomSeed == -1 ? 1 : randomSeed); 
-        mesh.vertices = vertices;
-        
-        //int[] triangles = new int[sizeX * sizeZ * 6];
-
-        
+        mesh.vertices = CreateVertices(target.position, sizeX, sizeZ, noiseScale, adjacentPointDistance, randomSeed == -1f ? 1f : randomSeed); 
+        mesh.triangles = CreateTriangles(mesh.vertices, sizeX, sizeZ);
         meshFilter.mesh = mesh;
+
         UnityEngine.MonoBehaviour.print("Mesh created.");
         return mesh;
     }
 
-    private static Vector3[] CreateVertices(Vector3 startPoint, int sizeX, int sizeZ, float noiseScale, float distanceBetweenVerts, int randomSeed){
-        Vector3[] vertices = new Vector3[(sizeX + 1) * (sizeZ + 1)];
+    private static int[] CreateTriangles(Vector3[] vertices, int sizeX, int sizeZ)
+    {
+        int[] triangles = new int[sizeX * sizeZ * 6];
+
+        int tIndex = 0;
+        int currentVertex = 0;
 
         for(int z = 0; z < sizeZ; z++){
             for(int x = 0; x < sizeX; x++){
-                int vertIndex = z * sizeX + x;
-                float xPos = startPoint.x + x * distanceBetweenVerts;
-                float zPos = startPoint.z + z * distanceBetweenVerts;
-                vertices[vertIndex] = new Vector3(xPos, GetVertexHeight(xPos * noiseScale, zPos * noiseScale, randomSeed), zPos);
+                triangles[tIndex + 0] = currentVertex;
+                triangles[tIndex + 1] = currentVertex + sizeX + 1;
+                triangles[tIndex + 2] = currentVertex + 1;
+                triangles[tIndex + 5] = currentVertex + sizeX + 2;
+                triangles[tIndex + 3] = currentVertex + 1;
+                triangles[tIndex + 4] = currentVertex + sizeX + 1;
+
+                tIndex += 6;
+                currentVertex++;
+            }
+            currentVertex++;
+        }
+        return triangles;
+    }
+
+    private static Vector3[] CreateVertices(Vector3 startPoint, int sizeX, int sizeZ, float noiseScale, float distanceBetweenVerts, float randomSeed){
+        Vector3[] vertices = new Vector3[(sizeX + 1) * (sizeZ + 1)];
+
+        for(int vertIndex = 0,z = 0; z < sizeZ + 1; z++){
+            for(int x = 0; x < sizeX + 1; x++){
+                float xPos = startPoint.x + (x * distanceBetweenVerts);
+                float zPos = startPoint.z + (z * distanceBetweenVerts);
+                vertices[vertIndex++] = new Vector3(xPos, GetVertexHeight(xPos, zPos, noiseScale, randomSeed), zPos);
             }
         }
 
         return vertices;
     }
 
-    private static float GetVertexHeight(float x, float z, int seed){
-        float yVal = Mathf.PerlinNoise(x * seed, z * seed);
+    private static float GetVertexHeight(float x, float z, float scale, float offset){
+        float yVal = Mathf.PerlinNoise((x + offset) * scale, (z + offset) * scale);
         return yVal;
     }
 
