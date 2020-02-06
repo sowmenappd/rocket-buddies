639444d6bbc88d0e3a951be78785373dfa794012
Sowmen Rahman <sowmen.appd@gmail.com>
2020-02-06 15:59:24 +0600

    implementing enemy state machine behavior

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,29 +2,136 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : LivingEntity{
+public class Enemy : LivingEntity {
 
-    public int damage;
+  public const float MAX_STAMINA = 100;
+  public float stamina;
+  public int damage;
 
-    protected override void Start(){
-        base.Start();
+  public float attackRange = 5f;
+
+  Weapon activeWeapon;
+  Transform player;
+
+  public enum State { Idle, Moving, Fleeing, Attacking, Reloading }
+  State state;
+
+  protected override void Start () {
+    base.Start ();
+    state = State.Idle;
+    stamina = MAX_STAMINA;
+    player = PlayerController.Instance.transform;
+
+    activeWeapon = transform.GetChild (0).GetChild (0).GetComponent<Weapon> ();
+    activeWeapon.Equip ();
+  }
+
+  void Update () {
+    if (alive && health <= 0) base.Die ();
+    if (!alive) return;
+
+    state = CalculateNextState (state);
+  }
+
+  State CalculateNextState (State state) {
+    if (state == State.Idle) {
+      if (stamina > 0) {
+        if (IsAttackIncoming (player)) {
+          return State.Fleeing;
+        } else {
+          return State.Moving;
+        }
+      } else {
+        if (IsTargetWithinAttackRange (player)) {
+          if (IsWeaponLoaded (activeWeapon)) {
+            return State.Attacking;
+          } else {
+            return State.Reloading;
+          }
+        } else {
+          if (stamina > 0) {
+            return State.Fleeing;
+          } else {
+            return State.Idle;
+          }
+        }
+      }
+
+    } else if (state == State.Moving) {
+      if (IsTargetWithinAttackRange (player)) {
+        if (IsWeaponLoaded (activeWeapon)) {
+          return State.Attacking;
+        } else {
+          return State.Reloading;
+        }
+      } else {
+        if (stamina > 0) {
+          return State.Fleeing;
+        } else {
+          return State.Idle;
+        }
+      }
+    } else if (state == State.Fleeing) {
+      if (stamina > 0) {
+        if (IsAttackIncoming (player)) {
+          return State.Fleeing;
+        } else {
+          return State.Moving;
+        }
+      } else {
+        if (IsTargetWithinAttackRange (player)) {
+          if (IsWeaponLoaded (activeWeapon)) {
+            return State.Attacking;
+          } else {
+            return State.Reloading;
+          }
+        } else {
+          if (stamina > 0) {
+            return State.Fleeing;
+          } else {
+            return State.Idle;
+          }
+        }
+      }
+    } else if (state == State.Attacking) {
+      if (stamina > 0) {
+        return State.Attacking;
+      } else {
+        return State.Idle;
+      }
     }
-
-    void Update(){
-        if(alive && health <= 0) base.Die();
-        if(!alive) return;
+    else if (state == State.Reloading){
+      if(stamina > 0){
+        return State.Fleeing;
+      } else {
+        return State.Idle;
+      }
     }
+    return state;
+  }
 
-    protected virtual void Fire(Transform target){
-        print("Enemy fired rockets.");
-    }
+  bool IsWeaponLoaded (Weapon weapon) {
+    return (weapon.currentAmmo > 0 && weapon.canFire && !weapon.switching);
+  }
 
-    public override void TakeDamage(int damage){
-        base.TakeDamage(damage);
-        print("Enemy dead.");
-    }
+  bool IsTargetWithinAttackRange (Transform attackTarget) {
+    return (attackTarget.position - transform.position).sqrMagnitude <= attackRange;
+  }
 
-    protected override void Die(){
-        alive = false;
-    }
+  bool IsAttackIncoming (Transform fromTarget) {
+    return false;
+  }
+
+  protected virtual void Fire (Transform target) {
+    print ("Enemy fired rockets.");
+  }
+
+  public override void TakeDamage (int damage) {
+    base.TakeDamage (damage);
+    print ("Enemy dead.");
+  }
+
+  protected override void Die () {
+    alive = false;
+  }
 }
