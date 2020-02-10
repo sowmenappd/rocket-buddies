dd9733dd4512fc6f982a26399b6ac38c4b947bad
Sowmen Rahman <sowmen.appd@gmail.com>
2020-02-10 15:48:48 +0600

    added debug option for disabling gizmos on spawnable objects to avoid scene cluttering and editor performance reduction

--- Assets/Scripts/Object Placement/SpawnableObject.cs
+++ Assets/Scripts/Object Placement/SpawnableObject.cs
@@ -12,19 +12,23 @@ public class SpawnableObject : MonoBehaviour, ISpawnable
 
     protected ISpawnable spawnable;
 
+    public static bool debug = true; 
+
     public virtual ISpawnable Spawn(Vector3 spawnPos)
     {
         //if(!spawnableObject) return new GameObject("NULL").AddComponent<ISpawnable>();
         GameObject obj = null;
         if(spawnableObject != null){
             obj = Instantiate(spawnableObject, spawnPos, transform.rotation);
+            obj.gameObject.name = spawnableObject.name;
             spawnable = obj.transform.GetComponent<ISpawnable>();
         }
         return spawnable;
     }
 
     void OnDrawGizmos(){
-        Gizmos.DrawWireSphere(transform.position + centerOffset, radius);
+        if(debug)
+            Gizmos.DrawWireSphere(transform.position + centerOffset, radius);
     }
 
     public void Destroy(){
